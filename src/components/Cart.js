8bfa5e2a572e18a66589a051382080775ea67def
malikrajat <er.rajatmalik@gmil.com>
2021-03-22 00:19:17 +0530

    update

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
 
 import { getCoupon } from "../store/actions/app.actions";
 
